5dae78dd57b257ab164aa3d860b03efca40eac79
itezjunhyeok <junhyeok@itez.io>
2023-01-20 15:59:57 +0900

    공동 구매 신청과 취소

--- src/pages/product-detail.tsx
+++ src/pages/product-detail.tsx
@@ -1,12 +1,16 @@
 import { gql, useMutation, useQuery } from '@apollo/client';
 import { format, parseISO } from 'date-fns';
+import React, { useState } from 'react';
 import { useParams } from 'react-router-dom';
 import Layout from 'src/components/layout';
 
 const ProductDetailPage = () => {
+  const [viewApplyBtn, setViewApplyBtn] = useState(true);
+
   const { id } = useParams();
   const groupPurchaseListId = Number(id);
 
+  // TODO 공동구매상품단일조회(모두)
   const SelectGroupPurchaseListByEveryone = gql`
     query SelectGroupPurchaseListByEveryone($groupPurchaseListId: Int!) {
       selectGroupPurchaseListByEveryone(groupPurchaseListId: $groupPurchaseListId) {
@@ -27,13 +31,58 @@ const ProductDetailPage = () => {
   const list = data?.selectGroupPurchaseListByEveryone;
   console.log(list);
 
+  // // TODO 회원조회(관리자)
+  // const SelectUserByAdmin = gql`
+  //   query SelectUserByAdmin($userId: Int!) {
+  //     selectUserByAdmin(userId: $userId) {
+  //       id
+  //       userPurchaseLog {
+  //         groupPurchaseListId
+  //       }
+  //     }
+  //   }
+  // `;
+  // const { data: userQuery } = useQuery(SelectUserByAdmin, { variables: { userId: 1 } });
+  // const userPLog = userQuery ? userQuery.userPurchaseLog : [];
+  // console.log('sss', userQuery?.userPurchaseLog);
+  // console.log('plg', userPLog.groupPurchaseListId);
+
+  // TODO 공동구매신청(회원)
   const ApplyGroupPurchaseByUser = gql`
     mutation ApplyGroupPurchaseByUser($groupPurchaseId: Int!) {
       applyGroupPurchaseByUser(groupPurchaseId: $groupPurchaseId)
     }
   `;
+  const [applyProduct] = useMutation(ApplyGroupPurchaseByUser);
 
-  const [applyByUser] = useMutation(ApplyGroupPurchaseByUser);
+  const groupPurchaseId = Number(id);
+  const onApply = () => {
+    applyProduct({ variables: { groupPurchaseId: groupPurchaseId } })
+      .then(res => {
+        console.log('res', res);
+        console.log('id', id);
+        alert('공동 구매 신청이 완료되었습니다.');
+        setViewApplyBtn(false);
+      })
+      .catch(err => console.log(err));
+  };
+
+  const CancelGroupPurchaseByUser = gql`
+    mutation CancelGroupPurchaseByUser($groupPurchaseId: Int!) {
+      cancelGroupPurchaseByUser(groupPurchaseId: $groupPurchaseId)
+    }
+  `;
+  const [cancelProduct] = useMutation(CancelGroupPurchaseByUser);
+  const onCancel = () => {
+    cancelProduct({ variables: { groupPurchaseId: groupPurchaseId } })
+      .then(res => {
+        console.log('resres', res);
+        console.log('idid', id);
+        alert('해당 상품의 신청이 취소되었습니다.');
+        setViewApplyBtn(true);
+      })
+      .catch(err => console.log(err));
+  };
 
   return (
     <Layout>
@@ -70,15 +119,25 @@ const ProductDetailPage = () => {
               <p>{list?.description}</p>
             </div>
           </div>
-          <button className='h-[62px] w-[241px] rounded-md bg-[#00c7ae]'>
-            <p className='font-[Roboto] text-[24px] font-semibold leading-[28.13px] text-[#ffffff]'>
+          {viewApplyBtn ? (
+            <button
+              onClick={onApply}
+              className=' h-[62px] w-[241px] rounded-md bg-[#00c7ae] font-[Roboto] text-[24px] font-semibold leading-[28.13px] text-[#ffffff]'
+            >
               신청하기
-            </p>
-          </button>
+            </button>
+          ) : (
+            <button
+              onClick={onCancel}
+              className=' h-[62px] w-[241px] rounded-md bg-[#323232] font-[Roboto] text-[24px] font-semibold leading-[28.13px] text-[#ffffff]'
+            >
+              신청취소
+            </button>
+          )}
         </div>
       </div>
     </Layout>
   );
 };
 
-export default ProductDetailPage;
+export default React.memo(ProductDetailPage);
